ed227851d0e0c6dd06241a78a42271a2011f69a7
Iurii Prymak <prymak@me.com>
2025-12-03 16:18:51 +0100

    Jupyter notebook RAG code moved to rag.py and fast.py

--- practicepreach/fast.py
+++ practicepreach/fast.py
@@ -1,8 +1,23 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from practicepreach.rag import Rag
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up...")
+    # e.g. connect to DB, load ML model, init client, create resources
+
+    app.state.rag = Rag()
+
+    yield
+
+    # e.g. close DB connection, free resources
+    print("Shutting down...")
+    app.state.rag.shutdown()
+
+app = FastAPI(lifespan=lifespan)
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -16,3 +31,14 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {'greeting': 'PracticePreach FastAPI is running!'}
+
+@app.get("/topics")
+def get_topics():
+
+    return {'topics': ['faith', 'hope', 'charity', 'love', 'forgiveness']}
+@app.get("/summaries")
+def get_summary(topic: str, daterange: str):
+
+
+    return {'party1': '**This party sucks!**',
+            'party2': '*This party is kinda okay.*'}
--- practicepreach/rag.py
+++ practicepreach/rag.py
@@ -14,80 +14,90 @@ import json
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.csv_loader import CSVLoader
-
-from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from .env file
-
-df = pd.read_csv("data/speeches-wahlperiode-21.csv")
-
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-model = init_chat_model("google_genai:gemini-2.5-flash-lite")
-
-prompt_template = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant. Use the following context to answer the question. Use maximum 7 sentences. Use specific terms. Highlight important ones."),
-    ("human", """Context: {context}  Question: {question}""")
-    ])
-
-example_messages = prompt_template.invoke(
-    {"context": "(context goes here)", "question": "(question goes here)"}
-).to_messages()
-
-vector_store = Chroma(
-    collection_name="example_collection",
-    embedding_function=embeddings,
-)
-
-loader = CSVLoader(file_path="data/speeches-wahlperiode-21.csv",
-                   metadata_columns=['date','id','party'])
-data = loader.load()
-
-#Shared text splitter for all document embedding
 from langchain_text_splitters import NLTKTextSplitter
 
-text_splitter = NLTKTextSplitter(
-    chunk_size=500,
-    chunk_overlap=200
-)
+from dotenv import load_dotenv
 
-def embed_and_store_pdf(file_path, vector_store, batch_size=200):
-    """Load a PDF file, split it into chunks, and store the chunks in a vector store."""
-    # Load the PDF file
-    loader = PyPDFLoader(file_path, mode="single")
-    pdf = loader.load()
+class RAG:
+    def __init__(self, vector_store, llm, prompt_template):
+        self.vector_store = vector_store
+        self.llm = llm
+        self.prompt_template = prompt_template
 
-    # Split the pages into chunks
-    all_splits = text_splitter.split_documents(pdf)
+        """Initialize environment variables and any other setup."""
+        load_dotenv()  # Load environment variables from .env file
 
-    # Add the party name to the metadata
-    pattern = r"(?<=data/)[^_]+(?=_)"
-    party_name = re.search(pattern, file_path)
+        df = pd.read_csv("data/speeches-wahlperiode-21.csv")
 
-    for split in all_splits:
-        split.metadata["party_name"] = party_name.group()
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+        model = init_chat_model("google_genai:gemini-2.5-flash-lite")
 
-    # Add the chunks to the vector store in batches
-    for i in range(0, len(all_splits), batch_size):
-        batch = all_splits[i:i + batch_size]
-        vector_store.add_documents(documents=batch)
+        prompt_template = ChatPromptTemplate.from_messages([
+            ("system", "You are a helpful assistant. Use the following context to answer the question. Use maximum 7 sentences. Use specific terms. Highlight important ones."),
+            ("human", """Context: {context}  Question: {question}""")
+        ])
 
-    return f"{file_path} embedded"
+        example_messages = prompt_template.invoke(
+            {"context": "(context goes here)", "question": "(question goes here)"}
+        ).to_messages()
 
-def answer(query, vector_store, llm, file, prompt_template=None):
-    """Answer a query using the vector store and the language model."""
-    # Retrieve similar documents from the vector store
-    retrieved_docs = vector_store.similarity_search(query,k=50, filter={'source': file})
+        vector_store = Chroma(
+            collection_name="example_collection",
+            embedding_function=embeddings,
+        )
 
-    # Create the prompt
-    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+        loader = CSVLoader(file_path="data/speeches-wahlperiode-21.csv",
+                   metadata_columns=['date','id','party'])
+        data = loader.load()
 
-    # If no prompt template is provided, use the default one
-    if not prompt_template:
-        prompt_template = hub.pull("rlm/rag-prompt")
+        text_splitter = NLTKTextSplitter(
+            chunk_size=500,
+            chunk_overlap=200
+        )
 
-    prompt = prompt_template.invoke(
-        {"context": docs_content, "question": query}
-    )
+    def embed_and_store_pdf(self, file_path, vector_store, batch_size=200):
+        """Load a PDF file, split it into chunks, and store the chunks in a vector store."""
+        # Load the PDF file
+        loader = PyPDFLoader(file_path, mode="single")
+        pdf = loader.load()
 
-    # Get the answer from the language model
-    answer = llm.invoke(prompt)
-    return answer.content
+        # Split the pages into chunks
+        all_splits = text_splitter.split_documents(pdf)
+
+        # Add the party name to the metadata
+        pattern = r"(?<=data/)[^_]+(?=_)"
+        party_name = re.search(pattern, file_path)
+
+        for split in all_splits:
+            split.metadata["party_name"] = party_name.group()
+
+        # Add the chunks to the vector store in batches
+        for i in range(0, len(all_splits), batch_size):
+            batch = all_splits[i:i + batch_size]
+            vector_store.add_documents(documents=batch)
+
+        return f"{file_path} embedded"
+
+    def answer(self, query, vector_store, llm, file, prompt_template=None):
+        """Answer a query using the vector store and the language model."""
+        # Retrieve similar documents from the vector store
+        retrieved_docs = vector_store.similarity_search(query,k=50, filter={'source': file})
+
+        # Create the prompt
+        docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+        # If no prompt template is provided, use the default one
+        if not prompt_template:
+            prompt_template = hub.pull("rlm/rag-prompt")
+
+        prompt = prompt_template.invoke(
+            {"context": docs_content, "question": query}
+     )
+
+        # Get the answer from the language model
+        answer = llm.invoke(prompt)
+        return answer.content
+
+    def shutdown(self):
+        """Clean up resources if needed."""
+        pass
