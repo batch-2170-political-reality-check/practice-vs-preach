afe18cc8f999e391d7408156e5fa147a3ff1cd75
Anna Chelsea Bahss <a.bahss@online.de>
2025-12-09 11:26:47 +0100

    Changed the params from expeceting an embedding to using the global param

--- practicepreach/wahlperiode_converter.py
+++ practicepreach/wahlperiode_converter.py
@@ -1,7 +1,7 @@
 from practicepreach.constants import BUNDESTAG_WAHLPERIODE
 
-def convert_to_wp_start(date, mapping):
-    for wp, (start, end) in mapping.items():
+def convert_to_wp_start(date):
+    for wp, (start, end) in BUNDESTAG_WAHLPERIODE.items():
         if start <= date <= end:
             return start
     return None
